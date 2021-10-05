7c2549867d9b94bf3cdf03ce7191128512b24b41
Esteban <esteban.github@gmail.com>
2021-10-05 10:02:46 +0200

    add styling to LovepieceDetails

--- client/src/pages/LovepieceDetails.js
+++ client/src/pages/LovepieceDetails.js
@@ -33,24 +33,44 @@ export default function LovepieceDetails(props) {
 
 	return (
 		<div>
-			{lovepiece && (
-				<>
 
-					<h2>{lovepiece.type} > {lovepiece.category} > {lovepiece.title}</h2>
-          <h4>Created by {lovepiece.owner.username}</h4>
-					<h4>Created on {formattedDateCreated}</h4>
-          <h6></h6>
-					<p>{lovepiece.description}</p>
-					<p>{lovepiece.tags}</p>
+			<div className="box">
 
-					{lovepiece.owner._id === user && (
-        		<Link to={`/lovepieces/edit/${lovepiece._id}`}>
-          		<button>Edit this lovepiece</button>
-        		</Link>
-      		)}
+					{lovepiece && (
+						<>
 
-				</>
-			)}
+							<div className="block">
+								<div className="media">
+									<div className="media-left">
+										<figure className="image is-48x48">
+											<img className="is-rounded" src="https://bulma.io/images/placeholders/96x96.png" alt="Placeholder image" />
+										</figure>
+									</div>
+									<div className="media-content">
+										<h4><strong>{lovepiece.owner.username}</strong></h4>
+										<h4>Lovepiece created on {formattedDateCreated}</h4>
+									</div>
+								</div>
+							</div>
+
+							<div className="box">
+							<h2 className="title is-5"><strong>{lovepiece.type} of {lovepiece.category}:</strong> {lovepiece.title}</h2>
+							<p>{lovepiece.description}</p>
+							<br /><br />
+							<p><strong>Tags: </strong>{lovepiece.tags}</p>
+							</div>
+
+							{lovepiece.owner._id === user && (
+								<Link to={`/lovepieces/edit/${lovepiece._id}`}>
+									<button className="button is-primary">Edit this lovepiece</button>
+								</Link>
+							)}
+
+						</>
+					)}
+
+				</div>
+			
 		</div>
 	);
 }
